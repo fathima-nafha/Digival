08b1d1c70a90b7c4466a7cedc410271a99791728
Fathima <nafhafathima67@gmail.com>
2019-04-12 10:20:55 +0530

    added qp

--- recognition/admin.py
+++ recognition/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Student, Teacher, QuestionBank
+from .models import Student, Teacher, QuestionBank, QuestionPaper
 # Register your models here.
 admin.site.register(Teacher)
 
 admin.site.register(Student)
 
 admin.site.register(QuestionBank)
+
+admin.site.register(QuestionPaper)
